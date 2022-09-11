dea1211d270441e48ddee1c27378a08bbab314ca
unknown <111202214659@mhs.dinus.ac.id>
2022-09-11 15:44:12 +0700

    first

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Muhammad Kelik Firmansyah" << endl;
+    cout << "A11.2022.14659" << endl;
+    cout << "SMK MIFTAHUL HUDA" << endl;
+    return 0;
+}
+
