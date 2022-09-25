478921eca89ed707d2887345afa44db83899369f
Firmansyah14659 <112918591+Firmansyah14659@users.noreply.github.com>
2022-09-25 13:45:28 +0800

    Add files via upload

---
+++ main5.cpp
@@ -0,0 +1,25 @@
+@@ -6,20 +6,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout <<"==========================================================================================="<< endl;
+    cout << "   \t\t\t PERTEMUAN 5 MEMBUAT PROGRAM LUAS LINGKARAN \t\t\t " << endl;
+    cout <<" \t\t\t\t\t\t\t\t\t\t\t " <<endl;
+    cout << "     NAMA : MUHAMMAD KELIK FIRMANSYAH , A11202214659 \t\t\t\t\t\t  " << endl;
+    cout <<"   \t\t\t\t\t\t\t\t\t\t\t " <<endl;
+    float n,r,rumus,hasil;
+    n = 3.14;
+    r = 10;
+    rumus= n*r*r;
+    cout <<"\t a. Masukan phi       \t: ";
+    cin >> n;
+    cout <<" \t b. Masukan jari-jari \t: ";
+    cin >> r;
+    cout <<" \t==> Masukan jari-jari \t: ";
+    cout <<" \t==> Masukan rumus \t: ";
+    cin >> rumus;
+    cout <<" \t--> Hasilnya adalah \t: " << n*r*r << endl;
+    cout <<"==========================================================================================="<<endl;
+    return 0;
+}
