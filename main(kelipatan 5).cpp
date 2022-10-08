2bd36d024b2710e10d6098415b03c76851f09672
Firmansyah14659 <112918591+Firmansyah14659@users.noreply.github.com>
2022-10-09 06:31:51 +0700

    Add files via upload

---
+++ main(kelipatan 5).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        int x;
+        cin >> x;
+
+        x =0;
+        do {
+            cout << x << endl;
+            x+=5;
+        }while (x <= 50);
+
+    return 0;
+}
